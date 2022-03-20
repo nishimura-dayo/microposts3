f3c58a660da0af716fd7ca847e283631839d405c
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2022-03-20 03:41:44 +0000

    post

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,12 +16,23 @@
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
                 {{-- ユーザ詳細タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">TimeLine</a></li>
+                <li class="nav-item">
+                    <a href="{{ route('users.show', ['user' => $user->id ])}}" class="nav-link {{ Request::routeIs('users.show') ? 'active' : '' }}">
+                        タイムライン
+                        <span class="badge badge-secondary">{{ $user->microposts_count }}</span>
+                    </a>
+                </li>
                 {{-- ユーザ一覧タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">フォローリスト</a></li>
                 {{-- ユーザ詳細タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">フォロワーリスト</a></li>
             </ul>
+            @if (Auth::id() == $user->id)
+                {{-- 投稿フォーム --}}
+                @include('microposts.form')
+            @endif
+            {{-- 投稿一覧 --}}
+            @include('microposts.microposts')
         </div>
     </div>
 @endsection
