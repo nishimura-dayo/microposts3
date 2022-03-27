328c3aa23abd019c386a2f961139ebc42b5b8a76
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2022-03-27 04:35:44 +0000

    navbar linkchenge

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -2,11 +2,11 @@
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
         <a class="navbar-brand" href="/">Microposts</a>
-        
+
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        
+
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
